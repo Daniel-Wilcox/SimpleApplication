ea80113f9952d1cb8713cbc13c35757125eef9ef
Dan <d.dt.wilcox@gmail.com>
2024-12-03 16:05:26 +0200

    added simple app to be used in Launcher

---
+++ app.py
@@ -0,0 +1,67 @@
+import json
+import os
+import tkinter as tk
+
+
+class TempView(tk.Frame):
+    def __init__(self, controller):
+        super().__init__(controller)
+
+        self.controller = controller
+
+        self.loading_title = tk.Label(
+            master=controller,
+            text=f"Main Application",
+        )
+        self.loading_title.pack(pady=20)
+
+        quit_button = tk.Button(
+            master=self, text="Quit", command=self.controller.destroy
+        )
+        quit_button.pack(padx=20, pady=10)
+
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        # Window properties
+        self.title("Application Launcher")
+        self.geometry("600x200")
+        self.eval("tk::PlaceWindow . center")
+        self.focus_force()
+
+        self.main_page = TempView(controller=self)
+        self.main_page.pack(side="top", fill="both", expand=True)
+        self.main_page.grid_rowconfigure(0, weight=1)
+        self.main_page.grid_columnconfigure(0, weight=1)
+        self.main_page.tkraise()
+
+
+def _check_config():
+    root_path = os.path.dirname(__file__)
+    config_path = os.path.join(root_path, "config.json")
+    data = {}
+    if not os.path.exists(config_path):
+        with open(config_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+    else:
+        try:
+            with open(config_path, "r") as f:
+                data = json.load(f)
+        except json.JSONDecodeError as e:
+            data = {}
+
+    return
+
+
+def main():
+    _check_config()
+
+    app = App()
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ make_application.py
@@ -0,0 +1,80 @@
+import json
+import os
+import platform
+import subprocess
+import sys
+import PyInstaller.__main__
+
+
+DEFAULT_CONFIG = {
+    "version": 1,
+    "app_path": "dist/app.app",
+    "github_url": f"https://github.com/Daniel-Wilcox/SimpleApplication",
+}
+
+
+def _read_json(filepath: str) -> dict:
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+    except json.JSONDecodeError as e:
+        return {}
+    return data
+
+
+def _write_config(config_path: str, data: dict):
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
+def _get_executable_path():
+    app_name = "app"
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    dist_path = os.path.join(root_dir, "dist", app_name)
+
+    match platform.system():
+        case "Windows":
+            executable_name = f"{app_name}.exe"
+        case _:
+            executable_name = f"{app_name}"
+
+    executable_path = os.path.join(dist_path, executable_name)
+
+    return executable_path
+
+
+def launch_executable():
+    exec_path = _get_executable_path()
+
+    if os.path.exists(exec_path):
+        print(f"Launching application from {exec_path}...")
+        subprocess.run([exec_path])  # Run the executable
+    else:
+        print(f"Executable not found at {exec_path}")
+
+    return ""
+
+
+def main():
+
+    root_path = os.path.dirname(__file__)
+    config_path = os.path.join(root_path, "config.json")
+
+    if not os.path.exists(config_path):
+        config_data = DEFAULT_CONFIG
+        _write_config(config_path, DEFAULT_CONFIG)
+        print(f"1. {config_data = }")
+
+    else:
+        config_data = _read_json(config_path)
+        print(f"2. {config_data = }")
+
+    # Pyinstaller
+    # PyInstaller.__main__.run(["app.py", "--onedir", "--windowed"])
+    # subprocess.call(r"pyinstaller app.py --onedir --windowed")
+
+    launch_executable()
+
+
+if __name__ == "__main__":
+    main()
